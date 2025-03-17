ac0e2133730d4a9a73da383c118d1c7ceffd9440
yıldırım <135065802+YILDIRIMT@users.noreply.github.com>
2025-03-17 19:00:27 +0300

    fix typo in bootse.s

--- src/boot/bootse.s
+++ src/boot/bootse.s
@@ -12,20 +12,20 @@ mov bx, info
 write_boot:
 	mov al, [bx]
 	cmp al, 0x00
-    je finish_boot
+    	je finish_boot
 	add bx, 1
 	int 0x10
 	jmp write_boot	
 
 finish_boot:
 	mov ah, 0x02 
-    mov al, 0x01 
-    mov ch, 0x00
-    mov cl, 0x02 
-    mov dh, 0x00
-    mov dl, 0x80 
-    mov bx, 0x8000
-    int 0x13
+    	mov al, 0x01 
+    	mov ch, 0x00
+    	mov cl, 0x02 
+    	mov dh, 0x00
+    	mov dl, 0x80 
+    	mov bx, 0x8000
+    	int 0x13
     
 	jmp 0x8000 ; LOAD SYSTEM
 
